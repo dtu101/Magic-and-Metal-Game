cdd9a2bdd2f51f43858f7837825b5117178e1f3a
dtu101 <doivicle@gmail.com>
2016-09-27 18:09:54 -0400

    Yayyy

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		
-		config.title = "Fullmetal ";
+		config.title = "Fullmetal Alchemist";
 		config.width = 1280;
 		config.height = 720;
 		
