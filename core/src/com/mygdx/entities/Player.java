8b0dc051b2ad42da0c842d26bdac58f67791579c
jiebinzhu <jiebinzhu1997@gmail.com>
2016-09-30 18:36:27 -0400

    Draw the guy

--- core/src/com/mygdx/entities/Player.java
+++ core/src/com/mygdx/entities/Player.java
@@ -1,6 +1,9 @@
 package com.mygdx.entities;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 public class Player extends Actor{
 	Integer health;
@@ -8,9 +11,21 @@ public class Player extends Actor{
 	Integer energy;
 	Integer experience;
 	String name;
+	Texture picture;
+	
 	
 	public Player(String s){
+		picture = new Texture("assets/Player.png");
 		name = s;
 	}
+	
+	public void render(SpriteBatch batch){
+		batch.begin();
+		batch.draw(picture, x, y);
+		
+		
+		batch.end();
+	}
+	
 
 }
--- core/src/com/mygdx/game/MainGame.java
+++ core/src/com/mygdx/game/MainGame.java
@@ -5,15 +5,19 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.entities.Player;
 
 public class MainGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
-	@Override
+	Player p;
+	
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("assets/DungeonFloor1.png");
+		
+		p = new Player("Alex");
 	}
 
 	@Override
@@ -23,6 +27,7 @@ public class MainGame extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 640 - img.getWidth()/2,360 - img.getHeight()/2);
 		batch.end();
+	
 	}
 	
 	@Override
