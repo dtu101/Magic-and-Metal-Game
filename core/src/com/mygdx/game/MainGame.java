3e3d2c64c029fc626c90ac1ee744baf92bc40524
jiebinzhu <jiebinzhu1997@gmail.com>
2016-09-30 17:54:48 -0400

    Picture centered

--- core/src/com/mygdx/game/MainGame.java
+++ core/src/com/mygdx/game/MainGame.java
@@ -18,10 +18,10 @@ public class MainGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 640 - img.getWidth()/2,360 - img.getHeight()/2);
 		batch.end();
 	}
 	
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		
-		config.title = "Commit sync refresh?";
+		config.title = "Magic and Metal";
 		config.width = 1280;
 		config.height = 720;
 		
